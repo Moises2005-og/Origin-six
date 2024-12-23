478fcb60bdd279410da79b4f538aeac9c7ee9d1e
unknown <moisessebastiao2005@gmail.com>
2024-12-23 17:56:36 +0100

    feat services

--- src/components/servico/index.tsx
+++ src/components/servico/index.tsx
@@ -1,11 +1,20 @@
+'use client'
+
+import { Card } from "../card"
 import { Container } from "../container"
 
 export function Servico() {
     return(
         <Container>
-            <div>
-                <h2>Servicos</h2>
-                <p>Com mais de 10 anos no mercado, o Beautysalon já conquistou clientes de inúmeros países com seus tratamentos exclusivos e totalmente naturais</p>
+            <div className="flex flex-col items-center gap-5 mb-16">
+                <h2 className="text-[40px] font-bold">Servicos</h2>
+                <p className="mb-5">Com mais de 10 anos no mercado, o Beautysalon já conquistou clientes de inúmeros países com seus tratamentos exclusivos e totalmente naturais</p>
+                <div className="flex flex-col sm:flex-row gap-8 rounded-sm">
+                    <Card title="Terapia capilar" src="/icone-hair.svg" content="Terapia completa para couro cabeludo e fios, protegendo todos os tipos de cabelos, inclusive os longos e finos."/>
+                    <Card title="Cortes" src="/cissor.svg" content="A nossa equipe é repleta de profissionais renomados, famosos por lançarem tendências com cortes diferenciados e clássicos." />
+                    <Card title="Tratamentos" src="/products-icon.svg" content="O beautysalon conta com diversos tratamentos naturais e totalmente veganos, para qualquer tipo 
+de cabelo." />
+                </div>
             </div>
         </Container>
     )
---
+++ src/components/testimonials/index.tsx
@@ -0,0 +1,7 @@
+export function Testimonials() {
+    return(
+        <div>
+            <h2>Depoimentos de quem ja passou por aqui</h2>
+        </div>
+    )
+}
