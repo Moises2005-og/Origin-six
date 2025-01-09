4fa2b55e8ec9699d0c3b5f51eaaa9500cad50dd2
unknown <moisessebastiao2005@gmail.com>
2025-01-09 15:16:52 +0100

    wip on testimonials

--- src/components/testimonials cards/index.tsx
+++ src/components/testimonials cards/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 
 type testimonialsData = {
@@ -7,11 +8,11 @@ type testimonialsData = {
 
 export function TestimonialsCard({image, name}: testimonialsData) {
     return(
-        <div>   
+        <div className="flex flex-col gap-[20px]">   
             <Image src='/public/Aspas.svg' alt="" width={5} height={5}/>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas obcaecati error culpa nobis placeat fugit dolores soluta cupiditate, quod blanditiis accusamus tenetur iure qui odio. Officiis ea error non quam.</p>
             <div>
-                <Image src={image} width={32} height={32} alt=""/>
+                <Image src={image} width={32} height={32} alt="avatar" className="rounded-full"/>
                 <p>{name}</p>
             </div>
         </div>
--- src/components/testimonials/index.tsx
+++ src/components/testimonials/index.tsx
@@ -1,9 +1,13 @@
 'use client'
+import React from "react";
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css';
+import { TestimonialsCard } from "../testimonials cards";
 
 export function Testimonials() {
     return (
-        <div className="flex justify-center text-[25px] font-bold">
-            <h2 className="">Depoimentos de quem já passou por aqui</h2>
+        <div className="flex flex-col justify-center text-[25px] font-bold">
+            <h2 className="text-center">Depoimentos de quem já passou por aqui</h2>
         </div>
     );
 }
